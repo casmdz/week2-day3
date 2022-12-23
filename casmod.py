d5875f3d2f490b632ba641729d201a83daccbdf7
cas <119894136+casmdz@users.noreply.github.com>
2022-12-22 22:17:32 -0600

    recommit

---
+++ casmod.py
@@ -0,0 +1,17 @@
+def printName(name):
+    print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+
+# 1) Has a function to calculate the square footage of a house 
+# Formula: Length X Width == Area
+# 2) Has a function to calculate the circumference of a circle
+ 
+
+def sq_footage(length, width):
+    return length * width 
+# print(sq_footage(5, 10))
+
+def my_circumference(radius):
+    pi = 3.1415
+    return 2 * pi * radius 
+
+testing= 'it worked!'
